84b1f52bcf37f23237bad4a851a4562f250b3a81
struggyyy <115942178+struggyyy@users.noreply.github.com>
2023-12-07 19:06:31 +0100

    update

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace WzorceProjektowe
+namespace Observer
 {
     public enum Genre
     {
--- Visitor/Program.cs
+++ Visitor/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Odwiedzajacy
+namespace Visitor
 {
     public interface ICity
     {
