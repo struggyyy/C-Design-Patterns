16b5bf38234db76922b15586a0df1597b90d08d6
struggyyy <115942178+struggyyy@users.noreply.github.com>
2023-12-18 12:46:51 +0100

    State added

---
+++ State/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+interface Stan
+{
+    void alert();
+}
+
+class Powiadomienia
+{
+    private Stan aktualnyStan;
+
+    public Powiadomienia()
+    {
+        aktualnyStan = new Wibracja();
+    }
+
+    public void ustawStan(Stan stan)
+    {
+        aktualnyStan = stan;
+    }
+
+    public void alert()
+    {
+        aktualnyStan.alert();
+    }
+}
+
+class Dzwonek : Stan
+{
+    public void alert()
+    {
+        Console.WriteLine("dzwonek...");
+    }
+}
+
+class Wibracja : Stan
+{
+    public void alert()
+    {
+        Console.WriteLine("wibracja...");
+    }
+}
+
+class Wyciszenie : Stan
+{
+    public void alert()
+    {
+        Console.WriteLine("wyciszenie...");
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Powiadomienia powiadomienia = new Powiadomienia();
+
+        powiadomienia.alert();
+
+        powiadomienia.ustawStan(new Dzwonek());
+        powiadomienia.alert();
+
+        powiadomienia.ustawStan(new Wyciszenie());
+        powiadomienia.alert();
+
+        powiadomienia.ustawStan(new Wyciszenie());
+        powiadomienia.alert();
+
+        powiadomienia.ustawStan(new Wibracja());
+        powiadomienia.alert();
+    }
+}
